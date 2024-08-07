870904e511d2c76ab6d9c683b5397a6426977fc2
arch <tymon3568@gmail.com>
2024-08-07 15:15:34 +0700

    edit MAIN_URL

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,8 +2,8 @@
 import PocketBase from 'pocketbase';
 export let HOST_URL = import.meta.env.VITE_HOST_URL;
 export const pb = new PocketBase(HOST_URL);
-export let MAIN_URL ='localhost:5173'
+export let MAIN_URL = 'https://www.lemyfinest.com';
 export const variationOptions = [
-    { id: 'kxgy94zy2n3zhhb', value: '10ml' },
-    { id: 'gpuq0jq4ig5qz9o', value: '20ml' }
+	{ id: 'kxgy94zy2n3zhhb', value: '10ml' },
+	{ id: 'w0iwewi4kt8614c', value: '100ml' }
 ];
