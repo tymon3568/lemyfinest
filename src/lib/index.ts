83503cf5c2e41a79cc80609c5d9321c4b7945d8f
arch <tymon3568@gmail.com>
2024-08-07 18:02:08 +0700

    edit env 2

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,6 +1,7 @@
 // place files you want to import through the `$lib` alias in this folder.
+import { env } from '$env/dynamic/private';
 import PocketBase from 'pocketbase';
-export let HOST_URL = import.meta.env.VITE_HOST_URL;
+export let HOST_URL = env.VITE_HOST_URL;
 export const pb = new PocketBase(HOST_URL);
 export let MAIN_URL = 'https://www.lemyfinest.com';
 export const variationOptions = [
